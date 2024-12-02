ce8c442d947fc02850fdf1523a33f2b1d469253a
David McFadzean <davidmc@gmail.com>
2024-12-02 10:38:08 -0500

    Day 2

---
+++ 2024/d02/part2.py
@@ -0,0 +1,52 @@
+# https://adventofcode.com/2024/day/2
+
+lines = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+
+f = open('data', 'r')
+lines = f.read()
+
+def isSafe(report):
+    print(report)
+    diffs = [(report[i+1] - report[i]) for i in range(len(report)-1)]
+    print(diffs)
+    if diffs[0] < 1:
+        for d in diffs:
+            if d > -1:
+                return False
+            if d < -3:
+                return False
+        return True
+    else:
+        for d in diffs:
+            if d < 1:
+                return False
+            if d > 3:
+                return False
+        return True
+
+def problemDampner(report):
+    if isSafe(report):
+        return True
+
+    for i in range(len(report)):
+        if isSafe(report[:i] + report[i+1:]):
+            return True
+
+    return False
+
+safe = 0
+for line in lines.split('\n'):
+    if not line:
+        continue
+    report = [int(level) for level in line.split(' ')]
+    if problemDampner(report):
+        safe += 1
+
+print(safe)
